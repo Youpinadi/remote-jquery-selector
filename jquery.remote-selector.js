0eb541cf6088efd04872cb5b15b60d3bc941bb16
Nadir Kadem <nadir.kadem@gmail.com>
2013-06-09 18:08:16 +0200

    Remove some debug code

--- jquery.remote-selector.js
+++ jquery.remote-selector.js
@@ -1,7 +1,6 @@
 (function ( $ ) {
     $.remote = function(selector, callback) {
-        match = selector.match('(https?://[^ ]+)(?: ?(.*))?');
-        console.log(match);
+        var match = selector.match('(https?://[^ ]+)(?: ?(.*))?');
         if (match.length)
         {
             var url = match[1];
@@ -27,24 +26,3 @@
         return this;
     };
 }( jQuery ));
-
-
-function imdbTop($els)
-{
-    $els.each(function(){
-        var imdb = $(this).attr('href').replace('/title/', '').replace('/', '');
-        $('body').append('<img src="http://cdn.rszr.co/w=50&q=100&imdb=' + imdb + '" style="float:left">');
-    });
-}
-
-$.remote('http://www.imdb.com/chart/top?ref_=nb_mv_3_ch table a[href*="title/tt"]', imdbTop);
-
-
-// $.remote('http://www.imdb.com/chart/top?ref_=nb_mv_3_ch table a[href*="title/tt"]',
-//     function($els)
-//     {
-//       $els.each(function(){
-//         imdb = $(this).attr('href').replace('/title/', '').replace('/', '');
-//         $('body').append('<img src="http://cdn.rszr.co/w=50&q=100&imdb=' + imdb + '" style="float:left">');
-//       });
-//     });
