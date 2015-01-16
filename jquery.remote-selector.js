fd5126eb0bf95f09599e7bc8606a849d555c5444
Nadir Kadem <nadir.kadem@gmail.com>
2015-01-16 15:43:03 +0100

    better example

--- jquery.remote-selector.js
+++ jquery.remote-selector.js
@@ -1,28 +1,30 @@
 (function ( $ ) {
-    $.remote = function(selector, callback) {
-        var match = selector.match('(https?://[^ ]+)(?: ?(.*))?');
-        if (match.length)
-        {
-            var url = match[1];
-            var selector = match[2];
-            $.ajax({
-                url: 'http://dom-perignon.appspot.com/get?url=' + url,
-                dataType: 'jsonp'
-            }).then(function(response){
-                if (selector)
-                {
-                    callback($(response).find(selector));
-                }
-                else
-                {
-                    callback($('<div>').append(response));
-                }
-            });
-        }
-        else
-        {
-            callback($(selector));
-        }
-        return this;
+    $.remote = function(selectors) {
+        var deffereds = [];
+        selectors = selectors.split(',');
+        $.each(selectors, function(index, selector){
+            var deffered = new jQuery.Deferred();
+            deffereds.push(deffered);
+            var match = selector.match('(https?://[^ ]+)(?: ?(.*))?');
+            if (match.length)
+            {
+                var url = match[1];
+                var selector = match[2];
+                $.ajax({
+                    url: 'http://dom-perignon.appspot.com/get?url=' + url,
+                    dataType: 'jsonp'
+                }).then(function(response){
+                    if (selector)
+                    {
+                        deffered.resolve($(response).find(selector));
+                    }
+                    else
+                    {
+                        deffered.resolve(response);
+                    }
+                });
+            }
+        });
+        return $.when.apply(null, deffereds);
     };
 }( jQuery ));
